ae18c8556c85badbacc7b31bf389497441a08be7
lmilliken <millifly@gmail.com>
2020-11-16 01:57:46 -0700

    13.2 11/19 axios, signup

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
 
 import router from './routes.js'
+
+axios.defaults.baseURL = "https://cis410-fa20-rockwellapi.azurewebsites.net";
+
 
 new Vue({
   el: '#app',
