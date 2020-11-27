48f9b4385ac9ea3d780cf0fabfd8a2ec96efdfa5
lmilliken <millifly@gmail.com>
2020-11-27 13:09:17 -0700

    15.1 12/1 Vuex store, sign in

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
-
+import store from './store.js'
 import router from './routes.js'
 
 axios.defaults.baseURL = "https://cis410-fa20-rockwellapi.azurewebsites.net";
@@ -11,5 +11,6 @@ axios.defaults.baseURL = "https://cis410-fa20-rockwellapi.azurewebsites.net";
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
---
+++ src/store.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state:{
+        token: null,
+        user: null,
+        movies: []
+    },
+    mutations:{
+        storeTokenInApp(state, myToken){
+            state.token = myToken
+        },
+        storeUserInApp(state, myUser){
+            state.user = myUser 
+        }
+    },
+    actions:{
+
+    }
+})
