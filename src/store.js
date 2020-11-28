e54a6a1e1f8fc97abaa0e7efc0fdb1c38a25a4f9
lmilliken <millifly@gmail.com>
2020-11-27 20:36:53 -0700

    15.2 12/3 /movies

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
+import axios from 'axios';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -15,9 +15,19 @@ export default new Vuex.Store({
         },
         storeUserInApp(state, myUser){
             state.user = myUser 
+        },
+        storeMovies(state, myMovies){
+            state.movies = myMovies
         }
     },
     actions:{
-
+        getMovies({commit}){
+            axios.get('/movies')
+            .then((myResponse)=>{
+                console.log("response from getMovies action", myResponse);
+                commit('storeMovies', myResponse.data)
+            })
+            .catch(()=>{console.log("error in getMovies action")})
+        }
     }
 })
